df728108f809f4e009b9c63622b1ac54a34356d8
pangeran-bottor <pangeranbhp@gmail.com>
2021-05-17 21:40:43 +0700

    basic functionalities

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import './popup.css'
 
+interface Definition {
+  def_text?: string;
+}
+
+
+const getData = async (word: string) => {
+  const resp = await fetch(`http://kateglo.com/api.php?format=json&phrase=${word}`)
+  const data = await resp.json()
+  return data.kateglo.definition
+}
+
 const App: React.FC<{}> = () => {
+  const [definitions, setDefinitions] = useState([] as Array<Definition>)
+  const [word, setWord] = useState("")
+  const [searchWord, setSearchWord] = useState("")
+
+  useEffect(() => {
+    const fetchData = async (word: string) => {
+      const data: Array<Definition> = await getData(word)
+      console.log(data)
+      setDefinitions(data)
+    }
+    if (searchWord.length > 0) {
+      fetchData(searchWord)
+    } 
+  }, [searchWord])
+
+  const handleCariBtn = () => {
+    console.log("current word: " + word)
+    setSearchWord(word)
+  }
+
   return (
-    <div>
-      <img src="icon.png" />
-    </div>
+    <>
+      <input type="text" id="targetWord" onChange={(event) => setWord(event.target.value as string)}></input>
+      <button onClick={handleCariBtn}>Cari</button>
+      <div>
+        {
+          definitions.map((definition: Definition) => {
+            return (
+              <h1>{ definition.def_text }</h1>
+            )
+          })
+        }
+      </div>
+    </>
   )
 }
 

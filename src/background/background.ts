3f298e66be4469e5875cc0b25f1effc3b32109b0
pangeran-bottor <pangeranbhp@gmail.com>
2021-05-17 22:51:24 +0700

    context menu and loading/error handling

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,15 @@
 // TODO: background script
 chrome.runtime.onInstalled.addListener(() => {
   // TODO: on installed function
+  console.log('background on installed.')
+
+  chrome.contextMenus.create({
+    contexts: ['selection'],
+    title: 'Cari arti',
+    id: 'artiinExtension',
+  })
+
+  chrome.contextMenus.onClicked.addListener((event) => {
+    console.log(event.selectionText)
+  })
 })
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -7,6 +7,8 @@ interface Definition {
 }
 
 
+type FetchState = "start" | "loading" | "error" | "ready"
+
 const getData = async (word: string) => {
   const resp = await fetch(`http://kateglo.com/api.php?format=json&phrase=${word}`)
   const data = await resp.json()
@@ -17,26 +19,65 @@ const App: React.FC<{}> = () => {
   const [definitions, setDefinitions] = useState([] as Array<Definition>)
   const [word, setWord] = useState("")
   const [searchWord, setSearchWord] = useState("")
+  const [fetchState, setFetchState] = useState<FetchState>("start")
 
   useEffect(() => {
     const fetchData = async (word: string) => {
       const data: Array<Definition> = await getData(word)
       console.log(data)
       setDefinitions(data)
+      setFetchState("ready")
     }
     if (searchWord.length > 0) {
-      fetchData(searchWord)
+      fetchData(searchWord).catch((err) => {
+        console.log("Caugh error: ", err)
+        setFetchState("error")
+      })
     } 
   }, [searchWord])
 
   const handleCariBtn = () => {
     console.log("current word: " + word)
+    setFetchState("loading")
     setSearchWord(word)
+  }
+
+  const handleEnterPressed = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      setFetchState("loading")
+      setSearchWord(word)
+    } 
+  }
+
+  if (fetchState == "loading" || fetchState == "error") {
+    return (
+      <>
+      <input 
+        type="text" 
+        id="targetWord" 
+        onChange={(event) => setWord(event.target.value as string)}
+        onKeyDown={handleEnterPressed}
+      ></input>
+      <button onClick={handleCariBtn}>Cari</button>
+      <div>
+        {
+          fetchState == "loading" 
+          ? "Loading ..."
+          : 'Error: could not retrieve data.'
+        }
+      </div>
+    </>
+    )
   }
 
   return (
     <>
-      <input type="text" id="targetWord" onChange={(event) => setWord(event.target.value as string)}></input>
+      <input 
+        type="text" 
+        id="targetWord" 
+        onChange={(event) => setWord(event.target.value as string)}
+        onKeyDown={handleEnterPressed}
+      ></input>
       <button onClick={handleCariBtn}>Cari</button>
       <div>
         {

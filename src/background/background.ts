b9c2dd5f479d0e979b68b5ca36d7f9f829428994
pangeran-bottor <pangeranbhp@gmail.com>
2021-05-23 17:54:38 +0700

    add listener outside onInstalled

--- src/background/background.ts
+++ src/background/background.ts
@@ -14,15 +14,16 @@ chrome.runtime.onInstalled.addListener(() => {
   //   chrome.tabs.sendMessage
   // })
 
-  chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData, tab: chrome.tabs.Tab) => {
-    console.log("got message from: ")
-    console.log(info.pageUrl)
-    chrome.tabs.sendMessage(tab.id, info.selectionText, null)
-  })
+})
 
-  chrome.runtime.onMessage.addListener((msg: any, sender: chrome.runtime.MessageSender, sendResponse) => {
-    console.log(msg)
-    console.log(sender)
-    console.log(sendResponse)
-  })
+chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData, tab: chrome.tabs.Tab) => {
+  console.log("got message from: ")
+  console.log(info.pageUrl)
+  chrome.tabs.sendMessage(tab.id, info.selectionText, null)
+})
+
+chrome.runtime.onMessage.addListener((msg: any, sender: chrome.runtime.MessageSender, sendResponse) => {
+  console.log(msg)
+  console.log(sender)
+  console.log(sendResponse)
 })
